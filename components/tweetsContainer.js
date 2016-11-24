09e39010da976e95d5b95d053f4362ad511edde7
kfrn <knfrances@gmail.com>
2016-11-25 11:23:05 +1300

    added init on reducer

--- components/tweetsContainer.js
+++ components/tweetsContainer.js
@@ -14,5 +14,4 @@ const TweetsView = (props) => {
   </div>
 }
 
-
 module.exports = TweetsView
--- reducer.js
+++ reducer.js
@@ -6,6 +6,17 @@ module.exports = function reducer (state, action){
 
   switch (action.type) {
     case 'INIT':
+      newState.tweets.forEach((tweet) => {
+        if(tweet.vote >= 0){
+          newState.posTweets.push(tweet)
+        } else {
+          newState.negTweets.push(tweet)
+        }
+      })
+      return newState
+    case 'UP_VOTE':
+      return newState
+    case 'DOWN_VOTE':
       return newState
     default:
       return newState
