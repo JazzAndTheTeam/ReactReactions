bcf3774474b1ec601e34241f4fbc07d4ecb8082c
kfrn <knfrances@gmail.com>
2016-11-25 15:01:03 +1300

    Destructure object

--- reducer.js
+++ reducer.js
@@ -4,14 +4,14 @@ const loadTweets = require('./apiCalls').loadTweets
 
 module.exports = function reducer (state, action){
   let newState = clone(state)
-
-  switch (action.type) {
+  const {type, payload} = action
+  switch (type) {
     case 'LOADING_TWEETS':
       newState.loadTweets = true
       return newState
     case 'INIT':
       newState.loadTweets = false
-      newState.tweets = action.payload
+      newState.tweets = payload
       newState.tweets.forEach((tweet) => {
         if(tweet.votes >= 0){
           newState.posTweets.push(tweet)
@@ -22,13 +22,13 @@ module.exports = function reducer (state, action){
       return newState
     case 'UP_VOTE':
       newState.tweets.forEach((tweet) => {
-        if(tweet.id === action.payload && !tweet.upVoted && !tweet.downVoted){
+        if(tweet.id === payload && !tweet.upVoted && !tweet.downVoted){
           tweet.votes++
           tweet.upVoted = true
-        } else if (tweet.id === action.payload && tweet.upVoted) {
+        } else if (tweet.id === payload && tweet.upVoted) {
           tweet.votes--
           tweet.upVoted = false
-        } else if (tweet.id === action.payload && tweet.downVoted){
+        } else if (tweet.id === payload && tweet.downVoted){
           tweet.votes += 2
           tweet.upVoted = true
           tweet.downVoted = false
@@ -37,13 +37,13 @@ module.exports = function reducer (state, action){
       return newState
     case 'DOWN_VOTE':
       newState.tweets.forEach((tweet) => {
-        if(tweet.id === action.payload && !tweet.upVoted && !tweet.downVoted){
+        if(tweet.id === payload && !tweet.upVoted && !tweet.downVoted){
           tweet.votes--
           tweet.downVoted = true
-        } else if (tweet.id === action.payload && tweet.downVoted) {
+        } else if (tweet.id === payload && tweet.downVoted) {
           tweet.votes++
           tweet.downVoted = false
-        } else if (tweet.id === action.payload && tweet.upVoted){
+        } else if (tweet.id === payload && tweet.upVoted){
           tweet.votes -= 2
           tweet.upVoted = false
           tweet.downVoted = true
